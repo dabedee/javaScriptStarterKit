b13616d73b496138b5f14bd35f567caa43599686
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-19 02:19:58 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("Merhaba kodlama.io")
+
+let dolarDun=9.20
+
+let dolarBugun=9.30
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+const euroDun=11.2
+// hatalı kullanım euroDun=11
+console.log(euroDun)
+let konutKredileri= ["Konut kredisi ","Emlak konut kredisi ","Kamut konut kredisi"]
+console.log("<ul>")
+for(let i= 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+    { /* <ul>
+        <li> konut kredisi </li>
+    <li> emlak konut kredisi </li>
+    <li> kamu konut kredisi </li>
+
+
+</ul> */}
+console.log(konutKredileri)
+ 
